fdac3e6caf7f8a48043a109e7b43fa74baf76b79
nazrulwazir <nazrulhakimwazir@gmail.com>
2018-04-16 13:09:34 +0800

    add google analytic

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -153,4 +153,14 @@
 	@include('vendor.sweetalert.cdn') 
 	@include('vendor.sweetalert.view')
 	@yield('after_scripts')
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117607947-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-117607947-1');
+	</script>
+
 </html>
